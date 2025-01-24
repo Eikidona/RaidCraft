8bd6a4816b1c6f39c4a5d3a83e16ed9cd13ee6d1
Forget_Melody <2734970539@qq.com>
2025-01-25 02:37:49 +0800

    RaidCraft update

--- src/main/java/com/forget_melody/raid_craft/capabilities/patrol_manager/PatrolManagerProvider.java
+++ src/main/java/com/forget_melody/raid_craft/capabilities/patrol_manager/PatrolManagerProvider.java
@@ -2,14 +2,16 @@ package com.forget_melody.raid_craft.capabilities.patrol_manager;
 
 import com.forget_melody.raid_craft.capabilities.Capabilities;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class PatrolManagerProvider implements ICapabilityProvider {
+public class PatrolManagerProvider implements ICapabilitySerializable<CompoundTag> {
 	private final IPatrolManager patrolManager;
 	private final LazyOptional<IPatrolManager> lazyOptional;
 	
@@ -21,5 +23,15 @@ public class PatrolManagerProvider implements ICapabilityProvider {
 	@Override
 	public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
 		return Capabilities.PATROLLER_MANAGER.orEmpty(cap, lazyOptional).cast();
+	}
+	
+	@Override
+	public CompoundTag serializeNBT() {
+		return patrolManager.serializeNBT();
+	}
+	
+	@Override
+	public void deserializeNBT(CompoundTag nbt) {
+		patrolManager.deserializeNBT(nbt);
 	}
 }
